a7ce30d8c51b6311a16225d6fac95c6083e6f788
Estudiante <estudiante@pssc.utn.ac.cr>
2016-05-16 21:20:19 -0600

    crud

---
+++ ShopAdvisor/Controllers/PlacesController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShopAdvisor.Models;
+
+namespace ShopAdvisor.Controllers
+{
+    public class PlacesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Places
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.Places.ToListAsync());
+        }
+
+        // GET: Places/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Place place = await db.Places.FindAsync(id);
+            if (place == null)
+            {
+                return HttpNotFound();
+            }
+            return View(place);
+        }
+
+        // GET: Places/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Places/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,name,latitud,longitud")] Place place)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Places.Add(place);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(place);
+        }
+
+        // GET: Places/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Place place = await db.Places.FindAsync(id);
+            if (place == null)
+            {
+                return HttpNotFound();
+            }
+            return View(place);
+        }
+
+        // POST: Places/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,name,latitud,longitud")] Place place)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(place).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(place);
+        }
+
+        // GET: Places/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Place place = await db.Places.FindAsync(id);
+            if (place == null)
+            {
+                return HttpNotFound();
+            }
+            return View(place);
+        }
+
+        // POST: Places/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            Place place = await db.Places.FindAsync(id);
+            db.Places.Remove(place);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
